40d3c8778586ace624b1f958cd91fa6edbdcff11
Bryan <b.fiallos@uvax.es>
2025-07-30 15:05:07 +0200

    primeras pruebas

---
+++ Module_1/first_program.py
@@ -0,0 +1,36 @@
+# Print hello
+print('Hello World!')
+boole = bool(1)
+print(type(12.0))
+print(boole)
+print("0123456".find('1'))
+
+
+d = "ABCDEFG"
+print("find: " + str(d.find("D")))
+pattern = "D"
+sep = d.split(pattern)
+print(sep)
+sepp = d[0:3]
+print(sepp)
+
+e = 'clocrkr1e1c1t'
+print(e[::2])
+print("\\")
+
+
+f = "You are wrong"
+f = f.lower()
+print(f)
+
+
+g = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+
+print(g.find("snow"))
+
+re = "."
+
+gr = g.replace(",", re)
+print(gr)
